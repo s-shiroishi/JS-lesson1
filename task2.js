8c0a4ee13e33311bddb5d099fb1573a440c02e90
shiro0114 <sugu01140114@gmail.com>
2024-03-12 23:34:17 +0900

    create task2.js

---
+++ task2.js
@@ -0,0 +1,18 @@
+while(true){
+
+    let userInput = prompt('日本の首都は？');
+
+    if(!userInput && userInput !== '')
+    {
+        break;
+    }
+    else if(userInput === '東京')
+    {
+        alert('正解です！');
+    }
+    else
+    {
+        alert('不正解です！');
+    }
+
+}
